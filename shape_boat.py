bb20f053a5c46a967791fe52e7b9d30891d2f73f
Banti Gheneti <banti@gheneti.com>
2019-01-15 22:56:04 -0500

    working MIQP w/ line and spline cleaned up + results

--- shape_boat.py
+++ shape_boat.py
@@ -7,74 +7,7 @@ from multiboat_trajectory_optimization.trajectory_planner import *
 from shapely.geometry import Point
 import numpy as np
 
-class ShapeBoat(TwoInputBoat, object):
-    
-    linear=True
-    
-    def __init__(self, boat_shape, obstacle_shape, margin=0):
-        self.shape = boat_shape
-        self.obstacle_shape = obstacle_shape
-        self.hulls = self.obstacle_shape.c_space(boat_shape,margin=margin).partition_around(self.shape, buffered=False)
-        self.g = HullGraph({(0,360):self.hulls})
-        self.hull_path = None
-    
-    def toProblemStates(self, boats_S):
-        assert len(boats_S)==1
-        return super(ShapeBoat, self).toProblemStates(boats_S)
-    
-    def toGlobalStates(self, boats_S, state_initial=None):
-        assert len(boats_S)==1
-        return super(ShapeBoat, self).toGlobalStates(boats_S, state_initial)
-
-    def set_end_points(self, x0, xN):
-        self.hull_path = self.g.point_path(Point(x0[0,:2]), Point(xN[0,:2])) 
-
-    def hull_constraint(self, h_bool, x, eq, M=100., mp=None):
-        val1 = M*(h_bool-1)+eq['b']
-        val2 = eq['A'].dot((x).T)
-        return mp.add_leq_constraints(val1, val2, linear=True) if mp is not None else val1<=val2
-        
-    def add_collision_constraints(self, mp, boats_S, M=20.):
-        assert len(boats_S)==1
-            
-        T = len(boats_S[0])
-        H = len(self.hull_path) 
-        self.in_hull = mp.NewBinaryVariables(T, H, "c")
-        
-        for t in range(T):
-            mp.AddLinearConstraint(np.sum(self.in_hull[t])==1)
-            boat_s = boats_S[0][t]
-            for i,h in enumerate(self.hull_path):
-                
-                self.hull_constraint(self.in_hull[t][i],                                       \
-                                     boats_S[0,t,:2],                                          \
-                                     self.g.vertex_properties["polygon_eq"][self.g.vertex(h)], \
-                                     mp=mp                                                     \
-                                    )
-                if t<T-1:
-                    if i>1:
-                        mp.AddLinearConstraint(2*(1-self.in_hull[t][i])>=self.in_hull[t+1][i-1])
-                    if i<H-1:
-                        mp.AddLinearConstraint((self.in_hull[t][i]-1)+1<=self.in_hull[t+1][i]+self.in_hull[t+1][i+1])
-                    if i==H-1:
-                        mp.AddLinearConstraint((self.in_hull[t][i]-1)+1<=self.in_hull[t+1][i])                        
-                        
-        mp.AddLinearConstraint(self.in_hull[0][0]==1)
-        mp.AddLinearConstraint(self.in_hull[-1][-1]==1)        
-                
-    @classmethod
-    def boat_dynamics(cls, s, u):
-
-        derivs = np.zeros_like(s)
-        
-        derivs[0] = s[2];
-        derivs[1] = s[3];
-        derivs[2] = u[0]-cls.d11 / cls.m11 * s[2] + u[0] / cls.m11;
-        derivs[3] = u[1]-cls.d22 / cls.m22 * s[3] + u[1] / cls.m22;
-        return derivs
-
-class ShapeBoat_spline(ThreeInputBoat, object):
-    
+class ShapeBoat(ThreeInputBoat, object):
     linear=True
     num_inputs=4
     
@@ -87,13 +20,13 @@ class ShapeBoat_spline(ThreeInputBoat, object):
         self.path      = None
         self.hull_path = None
     
-    def toProblemStates(self, boats_S):
-        assert len(boats_S)==1
-        return super(ShapeBoat_spline, self).toProblemStates(boats_S)
+    def toProblemStates(self, S):
+        assert len(S)==1
+        return super(ShapeBoat, self).toProblemStates(S)
     
-    def toGlobalStates(self, boats_S, state_initial=None):
-        assert len(boats_S)==1
-        return super(ShapeBoat_spline, self).toGlobalStates(boats_S, state_initial)
+    def toGlobalStates(self, S, state_initial=None):
+        assert len(S)==1
+        return super(ShapeBoat, self).toGlobalStates(S, state_initial)
 
     def set_end_points(self, x0, xN):
         self.msums, self.hulls = self.obstacle_shape.c_space_rotate(self.shape, x0[0], xN[0])
@@ -109,111 +42,208 @@ class ShapeBoat_spline(ThreeInputBoat, object):
                             "polygon_eq": self.g.vertex_properties["polygon_eq"][i], \
                             "min_angle":  self.g.vertex_properties["min_angle"][i],  \
                             "max_angle":  self.g.vertex_properties["max_angle"][i]} for i in self.path]
-        
-        print len(self.path)
 
-    def hull_constraint(self, h_bool, x, eq, M=100., mp=None):
+    @staticmethod        
+    def hull_constraint(h_bool, x, eq, M=100., mp=None):
         val1 = M*(h_bool-1)+eq['b']
         val2 = eq['A'].dot((x[:2]).T)
         return mp.add_leq_constraints(val1-val2,np.zeros(val1.shape), linear=True) if mp is not None else val1<=val2
     
-    def angle_constraint(self, h_bool, x, min_angle, max_angle, M=360, mp=None):
+    @staticmethod
+    def angle_constraint(h_bool, x, min_angle, max_angle, M=360, mp=None):
         val1 = M*(h_bool-1)+min_angle
         val2 = x[2]
         val3 = M*(1-h_bool)+max_angle
         return mp.add_leq_constraints([val1-val2, -val3+val2],[0, 0], linear=True) if mp is not None else val1<=val2<=val3
     
-    def angular_velocity_constraint(self, h_bool, x, min_angle, max_angle, M=360, mp=None):
+    @staticmethod
+    def angular_velocity_constraint(h_bool, x, min_angle, max_angle, M=360, mp=None):
         val1 = M*(h_bool-1)+min_angle-max_angle
         val2 = x[5]
         val3 = M*(1-h_bool)+max_angle-min_angle
         return mp.add_leq_constraints([val1-val2, -val3+val2],[0, 0], linear=True) if mp is not None else val1<=val2<=val3
 
-    def edge_line_constraint(self, e_bool, x0, x1, x2, M=100., mp=None):
-        const = M*(e_bool-np.ones(2))
-        x_dif = x1+(x1-x0)-x2
-                
-        val1 = np.concatenate((const, x_dif))
-        val2 = np.concatenate((x_dif, -const))
-        return mp.add_leq_constraints(val1-val2, np.zeros(val1.shape), linear=True) if mp is not None else val1==val2    
-    
-    def add_collision_constraints(self, mp, boats_S, in_hull, on_edge, M=20., opt_hull=True):
-        print "opt_hull", opt_hull
-        assert len(boats_S)==1
+    @staticmethod
+    def add_integer_constraints(in_hull, on_edge, angle_mod, mp):
+        N, H = in_hull.shape  
+
+        mp.AddLinearConstraint(in_hull[0][0]==1)
+        mp.AddLinearConstraint(in_hull[-1][-1]==1)   
         
-        T = len(boats_S[0])
-        H = len(self.hull_path)
-                    
-        for t in range(T-1):
-            if opt_hull:
-                mp.AddLinearConstraint(np.sum(in_hull[t])==1)
-                        
-            #integer based constraint for edges to lie in hulls 
-            for i,hull in enumerate(self.hull_path):  
-                
-                #for current and next knot point in segment
-                for dt in range(2):
-                    self.hull_constraint(in_hull[t][i],              \
-                                         boats_S[0,t+dt],            \
-                                         hull["polygon_eq"],         \
-                                         mp=mp                       \
-                                        )
-                    
-                    self.angle_constraint(in_hull[t][i],             \
-                                         boats_S[0,t+dt],            \
-                                         hull["min_angle"],          \
-                                         hull["max_angle"],          \
-                                         mp=mp                       \
-                                        )       
-                    
-                    self.angular_velocity_constraint(in_hull[t][i],  \
-                                         boats_S[0,t+dt],            \
-                                         hull["min_angle"],          \
-                                         hull["max_angle"],          \
-                                         mp=mp                       \
-                                        )                                       
-                #reduce searchspace if traversing hulls sequentially
-                if t<T-2 and opt_hull:
+        for t in range(N-1):
+            for i in range(H):  
                     if i>1:
                         mp.AddLinearConstraint(2*(1-in_hull[t][i])>=in_hull[t+1][i-1])
                     if i<H-1:
                         mp.AddLinearConstraint((in_hull[t][i]-1)+1<=in_hull[t+1][i]+in_hull[t+1][i+1])
                     
             #reduce searchspace if traversing hulls sequentially
-            if t<T-2 and opt_hull:
-                mp.AddLinearConstraint(in_hull[t][H-1]<=in_hull[t+1][H-1])
+            mp.AddLinearConstraint(in_hull[t][H-1]<=in_hull[t+1][H-1])
+        for t in range(1,N):            
+            for i in range(H):
+                mp.AddLinearConstraint(in_hull[t-1][i]<=in_hull[t][i]+on_edge[t-1])
 
-        if opt_hull:
-            for t in range(1,T-1):
-                t_e = t-1
+    def add_position_collision_constraints(self, S, in_hull, on_edge, mp, M=20.):
+        for t in range(S.shape[1]-1):
+            for i,hull in enumerate(self.hull_path):
+                for dt in range(2):
+                    self.hull_constraint(
+                                         in_hull[t][i],      \
+                                         S[0,t+dt],          \
+                                         hull["polygon_eq"], \
+                                         mp=mp               \
+                                        )
 
-                #if current segment in hull, ensure next segment in hull or traversing edge between hull
-                for i in range(len(self.hull_path)):
-                    mp.AddLinearConstraint(in_hull[t-1][i]<=in_hull[t][i]+on_edge[t_e][0])
+    def add_angle_collision_constraints(self, S, in_hull, mp, M=20.):
+        for t in range(S.shape[0]-1):
+            for i,hull in enumerate(self.hull_path):  
+                for dt in range(2):
+                    cls.angle_constraint(
+                                         in_hull[t][i],      \
+                                         S[0,t+dt],          \
+                                         hull["min_angle"],  \
+                                         hull["max_angle"],  \
+                                         mp=mp               \
+                                        )       
 
-                self.edge_line_constraint(on_edge[t_e][0],      \
-                                          boats_S[0,t-1,:2],    \
-                                          boats_S[0,t,:2],      \
-                                          boats_S[0,t+1,:2],    \
-                                          mp=mp                 \
-                                         )
-
-            mp.AddLinearConstraint(in_hull[0][0]==1)
-            mp.AddLinearConstraint(in_hull[-1][-1]==1)    
-                
-    @classmethod
-    def boat_dynamics(cls, s, u):
-
+                    cls.angular_velocity_constraint(                                         
+                                         in_hull[t][i],      \
+                                         S[0,t+dt],          \
+                                         hull["min_angle"],  \
+                                         hull["max_angle"],  \
+                                         mp=mp               \
+                                        )
+                                       
+    @staticmethod
+    def boat_dynamics(s, u, am):
         derivs = np.zeros_like(s)
+        derivs[0] = s[3]
+        derivs[1] = s[4]
+        derivs[2] = s[5] + .375*u[2] + .125*u[3] + 360*(am[0]-am[1])
+        derivs[3] = u[0]
+        derivs[4] = u[1]
+        derivs[5] = .5*u[2] + .5*u[3]
+        return derivs
+                                       
+    @classmethod
+    def add_transition_constraints(cls, S, U, angle_mod, mp):
+        for k in range(1,S.shape[1]):        
+            u0 = U[0,k-1] #old input
+            s0 = S[0,k-1] #old state
+            s  = S[0,k]   #new state
+            am = angle_mod[k-1]
+
+            #State transition constraint
+            mp.add_equal_constraints(s, s0 + cls.boat_dynamics(s0, u0, am),linear=cls.linear)
+                                       
+    @staticmethod
+    def add_input_position_cost(S, U, mp):
+        return mp.AddQuadraticCost(np.sum(U[0,:,:2]**2))
+
+    @staticmethod
+    def add_input_angle_cost(S, U, mp):
+        return mp.AddQuadraticCost(np.sum((U[0,:,2:]/100)**2))
+
+
+class ShapeBoat_spline(ShapeBoat, object):
+    
+    M = 0.5 * np.array([[1, 1, 0],[-2, 2, 0],[1, -2, 1]])
+    p_cost_matrix = M.T.dot(np.array([[0, 0, 2]]).T.dot(np.array([[0, 0, 2]])).dot(M))
         
-        derivs[0] = s[2];
-        derivs[1] = s[3];
-        derivs[2] = u[0]-cls.d11 / cls.m11 * s[2] + u[0] / cls.m11;
-        derivs[3] = u[1]-cls.d22 / cls.m22 * s[3] + u[1] / cls.m22;
+    @classmethod
+    def B(cls, u_t):
+        return np.array([1, u_t, u_t**2]).dot(cls.M)
+    
+    @classmethod    
+    def dB_dt(cls, u_t, dN):
+        return np.array([0, 1, 2*u_t]).dot(cls.M)/dN
+    
+    @classmethod    
+    def d2B_dt2(cls, u_t, dN):
+        return np.array([0, 0, 2]).dot(cls.M)/dN**2
+    
+    @classmethod
+    def knots_to_trajectory(cls, S, U, dN, order=3):
+        S_sample = np.zeros((S.shape[0],S.shape[1]+(order-1)+(order-2),S.shape[2]))
+        U_sample = np.zeros((U.shape[0],U.shape[1]+2,U.shape[2]))
+        S_sample[:,order-1:-order] = S[:,1:-1]
+        S_sample[:,:order-1] = S[:,0,:]
+        S_sample[:,-order:] = S[:,-1,:]
+        U_sample[:,:-2] = U
+
+        shape = S_sample.shape   
+        N = dN*(shape[1]-3)+1
+        S_new = np.zeros((1,N,shape[2]))
+        U_new = np.zeros((1,N,3))
+
+        for x in range(N): 
+            knot_ind = int(x/dN)
+            knot_fraction = x/float(dN)-knot_ind
+            p = S_sample[0,knot_ind:knot_ind+3,:2]
+
+            S_new[0,x,:2]  = cls.B(knot_fraction).dot(p)
+            S_new[0,x,3:5] = cls.dB_dt(knot_fraction, dN).dot(p)
+            U_new[0,x,:2]  = cls.d2B_dt2(knot_fraction, dN).dot(p)           
+            U_new[0,x,2]   = U_sample[0,knot_ind,2] if knot_fraction<0.5 else U_sample[0,knot_ind,3]
+
+        return S_new, U_new
+    
+    @staticmethod                                           
+    def edge_line_constraint(e_bool, x0, x1, x2, M=100., mp=None):
+        const = M*(e_bool-np.ones(2))
+        x_dif = x1+(x1-x0)-x2
+                                       
+        val1 = np.concatenate((const, x_dif))
+        val2 = np.concatenate((x_dif, -const))
+        return mp.add_leq_constraints(val1-val2, np.zeros(val1.shape), linear=True) if mp is not None else val1==val2  
+
+    def add_position_collision_constraints(self, S, in_hull, on_edge, mp, M=20.):
+        super(ShapeBoat_spline, self).add_position_collision_constraints(S, in_hull, on_edge, mp, M)
+                 
+        for t in range(1,S.shape[1]-1):
+            t_e = t-1
+            self.edge_line_constraint(on_edge[t_e],    \
+                                      S[0,t-1,:2],     \
+                                      S[0,t,:2],       \
+                                      S[0,t+1,:2],     \
+                                      mp=mp            \
+                                     )
+
+    @classmethod    
+    def add_input_position_cost(cls, S, U, mp):
+        T = S.shape[1]
+        for k in range(T):
+            P = np.zeros((3,2),S.dtype)
+            if k==0:
+                P[0] = P[1] = S[0,0,:2]
+                P[2] = S[0,k+1,:2]
+            elif k==T-1:
+                P[0] = S[0,k-1,:2]
+                P[1] = P[2] = S[0,T-1,:2]
+            else:
+                P=S[0,k-1:k+2,:2]
+            mp.AddQuadraticCost(np.sum(np.multiply(cls.p_cost_matrix.dot(P),P)))
+
+    @staticmethod              
+    def boat_dynamics(s, u, am):
+        derivs = np.zeros_like(s)
+        derivs[2] = s[5] + .375*u[2] + .125*u[3] + 360*(am[0]-am[1])
+        derivs[5] = .5*u[2] + .5*u[3]
         return derivs
     
-    
-    
+    @classmethod                                                    
+    def add_transition_constraints(cls, S, U, angle_mod, mp):
+        for k in range(1,S.shape[1]):        
+            u0 = U[0,k-1] #old input
+            s0 = S[0,k-1] #old state
+            s  = S[0,k]   #new state
+            am = angle_mod[k-1]
+
+            #State transition constraint
+            mp.add_equal_constraints(s[[2,5]], s0[[2,5]] + cls.boat_dynamics(s0, u0, am)[[2,5]],linear=cls.linear)
+
+            
+            
 def check_vertex_constraints(boat):
     ##Check Contains Functions works properly
     X = (np.random.rand(100,2)-0.5)*8
@@ -226,6 +256,4 @@ def check_vertex_constraints(boat):
           
 def check_state_in_hull(path_hull_i, state_i):
     hull_i = boat.hull_path[path_hull_i]
-    print boat.g.vertex_properties['polygon'][boat.g.vertex(hull_i)].contains(Point(boats_S[0,state_i][:2]))
-
-        
+    print boat.g.vertex_properties['polygon'][boat.g.vertex(hull_i)].contains(Point(S[0,state_i][:2]))     
--- shapeshift_trajectory.py
+++ shapeshift_trajectory.py
@@ -2,9 +2,12 @@ from shape_boat import ShapeBoat_spline
 from pydrake.all import SolutionResult
 from multiboat_trajectory_optimization.trajectory_planner import BoatConfigurationPlanning
 import numpy as np
+import pickle
 
 def shapeshift_trajectory(boat_shape, obstacle_shape, xy0, xyN, N=15, margin=0.0, boat_type=ShapeBoat_spline, opt_angle=True):
     result2 = None
+    S_knots = None
+    U_knots = None
     
     x0 = np.zeros((1, boat_type.num_states))
     xN = np.zeros((1, boat_type.num_states))
@@ -14,61 +17,30 @@ def shapeshift_trajectory(boat_shape, obstacle_shape, xy0, xyN, N=15, margin=0.0
     boat.set_end_points(x0, xN)
     
     planner = BoatConfigurationPlanning(boat)
-    S_knots, U, in_hull, on_edge, mp, result1, solve_time = planner.compute_spline_trajectory(0., 10, x0, xN, input_position_cost=True, slack=0, N=N)
+    S, U, in_hull, on_edge, mp, result1, solve_time = planner.compute_spline_trajectory(x0, xN, opt_angle=False, N=N)
     success = result1==0
         
     if opt_angle and success:
-        S_knots, U_knots, in_hull, on_edge, mp, result2, solve_time = planner.compute_spline_trajectory(0., 10, x0, xN, input_angle_cost=True, slack=0, N=N, in_hull=in_hull, on_edge=on_edge, states_initialization=S_knots, fix_initialization_inds=[0,1])
+        S, U, in_hull, on_edge, mp, result2, solve_time = planner.compute_spline_trajectory(x0, xN, opt_position=False, N=N, in_hull=in_hull, on_edge=on_edge, S_initialization=S, S_fix_inds=[0,1])
         success = result2==0
-               
-    S, U = knots_to_trajectory(S_knots, U_knots, 10) if success else None
-    
+
+    if boat_type is ShapeBoat_spline:
+        S_knots = S
+        U_knots = U
+        S, U = boat_type.knots_to_trajectory(S_knots, U_knots, 40) if success else (None,None)   
+                   
     return {'boat':          boat, 
             'S_knots':       S_knots,
             'U_knots':       U_knots,
             'S' :            S,
             'U':             U, 
+            'in_hull':       in_hull,
+            'on_edge':       on_edge,
             'result_spline': result1,
             'result_angle':  result2,
             'success':       success,
             'metrics':       metrics(U) if success else None
             }
-
-
-def knots_to_trajectory(S, U, dN, order=3):
-    S_sample = np.zeros((S.shape[0],S.shape[1]+(order-1)+(order-2),S.shape[2]))
-    U_sample = np.zeros((U.shape[0],U.shape[1]+2,U.shape[2]))
-    S_sample[:,order-1:-order] = S[:,1:-1]
-    S_sample[:,:order-1] = S[:,0,:]
-    S_sample[:,-order:] = S[:,-1,:]
-    U_sample[:,:-2] = U
-        
-    shape = S_sample.shape
-    num_knots = shape[1]
-    
-    #number of knots
-    N = dN*(num_knots-3)+1
-    
-    S_new = np.zeros((shape[0],N,shape[2]))
-    U_new = np.zeros((shape[0],N,3))
-
-    M = 0.5 * np.array([[1, 1, 0],[-2, 2, 0],[1, -2, 1]])
-                    
-    for b in range(shape[0]):
-        for x in range(N): 
-            knot_ind = int(x/dN)
-            knot_fraction = x/float(dN)-knot_ind
-            p = S_sample[b,knot_ind:knot_ind+3,:2]
-            B = np.array([1, knot_fraction, knot_fraction**2]).dot(M)
-            dB_dt = np.array([0, 1, 2*knot_fraction]).dot(M)/dN
-            d2B_dt2 = np.array([0, 0, 2]).dot(M)/dN
-                        
-            S_new[b,x,:2]  = B.dot(p)
-            S_new[b,x,3:5] = dB_dt.dot(p)
-            U_new[b,x,:2]  = d2B_dt2.dot(p)           
-            U_new[b,x,2]   = U_sample[b,knot_ind,2] if knot_fraction<0.5 else U_sample[b,knot_ind,3]
-            
-    return S_new, U_new
 
 def metrics(U):
     return analytics({'U_position_cost': U_position_cost(U), 'U_angle_cost': U_angle_cost(U)})
@@ -82,3 +54,7 @@ def U_position_cost(U):
 
 def U_angle_cost(U):
     return U[0,:,2]**2
+
+def write_experiment(results, label):
+    with open('results/MIQP_'+label+'.pickle', 'wb') as f:
+        pickle.dump(results, f)
